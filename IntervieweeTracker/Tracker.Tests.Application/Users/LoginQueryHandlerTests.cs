6d10feabfb20569986232aa6c962c3cfa7d4074e
Roman <romapavlenkod@gmail.com>
2024-04-12 10:50:22 +0300

    fix application login handler

--- IntervieweeTracker/Tracker.Tests.Application/Users/LoginQueryHandlerTests.cs
+++ IntervieweeTracker/Tracker.Tests.Application/Users/LoginQueryHandlerTests.cs
@@ -36,11 +36,6 @@ public class LoginQueryHandlerTests
                 It.Is<Email>(e => e.Value == userEmail.Value),
                 It.IsAny<CancellationToken>()))
             .Returns(Task.FromResult(user)!);
-        userRepositoryMock
-            .Setup(repository => repository.Login(
-                It.Is<User>(u => u.Email == userEmail && u.Name == userName && u.RoleId == userRoleId),
-                It.IsAny<CancellationToken>()))
-            .Returns(Task.CompletedTask);
 
         var tenantRepositoryMock = new Mock<ITenantRepository>(MockBehavior.Strict);
         tenantRepositoryMock
@@ -57,7 +52,7 @@ public class LoginQueryHandlerTests
             .Setup(tenantRepositoryFactory => tenantRepositoryFactory.GetTenant())
             .Returns(tenantRepositoryMock.Object);
 
-        var query = new LoginQuery(userEmail.Value);
+        var query = new LoginQuery(userEmail.Value, password);
         var sut = new LoginQueryHandler(tenantRepositoryFactoryMock.Object);
 
         // Act
@@ -80,6 +75,8 @@ public class LoginQueryHandlerTests
         var userRoleId = Guid.NewGuid();
         var userName = _fixture.Create<string>();
         var userEmail = _fixture.Create<Email>();
+        var password = _fixture.Create<string>();
+        var user = User.Create(userRoleId, userName, userEmail, password);
 
         var userRepositoryMock = new Mock<IUserRepository>(MockBehavior.Loose);
         userRepositoryMock
@@ -87,11 +84,6 @@ public class LoginQueryHandlerTests
                 It.Is<Email>(e => e.Value != userEmail.Value),
                 It.IsAny<CancellationToken>()))
             .Returns(value: null);
-        userRepositoryMock
-            .Setup(repository => repository.Login(
-                It.Is<User>(u => u.Email == userEmail && u.Name == userName && u.RoleId == userRoleId),
-                It.IsAny<CancellationToken>()))
-            .Returns(Task.CompletedTask);
 
         var tenantRepositoryMock = new Mock<ITenantRepository>(MockBehavior.Strict);
         tenantRepositoryMock
@@ -108,7 +100,7 @@ public class LoginQueryHandlerTests
             .Setup(tenantRepositoryFactory => tenantRepositoryFactory.GetTenant())
             .Returns(tenantRepositoryMock.Object);
 
-        var query = new LoginQuery(userEmail.Value);
+        var query = new LoginQuery(userEmail.Value, password);
         var sut = new LoginQueryHandler(tenantRepositoryFactoryMock.Object);
 
         // Act
